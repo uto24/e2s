668abf1292302ea728a61706f36eeb59517b4b51
uto24 <utechfficial24@gmail.com>
2025-11-29 11:07:01 +0600

    Change router from HashRouter to BrowserRouter

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, CartProvider, ShopProvider } from './services/store';
 import Layout from './components/Layout';
 import AdminLayout from './components/AdminLayout';
